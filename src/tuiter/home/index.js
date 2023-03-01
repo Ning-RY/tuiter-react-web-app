02e035ff9e056f53dd37509a5733fe0624a59c5b
lnyrlyed <yang.ruini@northeastern.edu>
2023-03-01 12:48:17 -0500

    a6 React.js sp23

---
+++ src/tuiter/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+import PostList from "../post-list/postList";
+
+const HomeComponent = () => {
+    return (
+        <>
+            <PostList/>
+        </>
+    );
+};
+export default HomeComponent;
